dec48dc0175c653eb8f457c6e645b0d1018314cc
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-21 09:50:20 +0900

    Create ReferenceType_190321.java

---
+++ Java/ReferenceType_190321.java
@@ -0,0 +1,29 @@
+class Example
+{
+	public static void main(String [] args){
+
+	String ex = "가나";
+
+	System.out.println(ex);
+
+	}
+}
+
+
+import java.util.Scanner;
+
+class Example2
+{
+	public static void main(String [] args){
+
+		int number;
+
+		Scanner sc = new Scanner(System.in);
+    
+    System.out.println("정수를 입력하세요 : ");
+		number = sc.nextInt();//키보드로 부터 정수 입력받기
+
+		System.out.println(number);
+
+	}
+}
