2f000b648e5f8c323bfec28a4272c246a1e62547
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-24 14:37:19 +0900

    Update 190320_Variable.java

--- Java/190320_Variable.java
+++ Java/190320_Variable.java
@@ -1,25 +1,24 @@
 
 //ex1
 class Example {
-
 	public static void main(String[] args) {
 //변수의 8가지 종류, 변수는 소문자로 시작한다
 		int number; //선언&대입
-    
-    number=24;
-    
-    System.out.println(number);
+		
+		number=24;
+	
+		System.out.println(number);
 
 	}
 }
 
 //ex2
 class Example {
-
+	
 	public static void main(String[] args) {
-
+		
 		int number=5;
-
+	
 		System.out.println(number/2);
 /*int&int 연산 = int
 int&double 연산 = 더 큰 타입으로 연산값 출력
@@ -29,15 +28,14 @@ int&double 연산 = 더 큰 타입으로 연산값 출력
 
 //ex3
 class Example {
-
+	
 	public static void main(String[] args) {
-
 		int numberOne=5;
-    int numberTwo=numberOne/2.0;
+		
+		int numberTwo=numberOne/2.0;
     //연산값이 double => double
 
 		System.out.println(number);
-
 	}
 }
 
@@ -50,23 +48,20 @@ class Example {
 		
 		++number;
 		
-	System.out.println(number)
-
+	System.out.println(number);
 	}
 }
 
 //ex5
 class Example {
-
 	public static void main(String[] args) {
 
 		int number=10;
-
-	System.out.println(number++);
-	System.out.println(number++);
-	System.out.println(++number);
-	System.out.println(number);
-
+		
+		System.out.println(number++);
+		System.out.println(number++);
+		System.out.println(++number);
+		System.out.println(number);
 	}
 }
 //선대입 후증가
@@ -75,6 +70,7 @@ class Example {
 class InDecrementOp
 {
 	public static void main(String[] args) {
+		
 		int cnt = 10;
 
 		System.out.println("cnt : "+ cnt);
@@ -88,6 +84,7 @@ class InDecrementOp
 class  Math
 {
 	public static void main(String[] args){
+		
 		int price=4300;
 		long number=2435398;
 		int mart=13;
