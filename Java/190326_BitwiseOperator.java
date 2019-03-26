9bf89a3df475bf433ef4f532ca86cb9cff461372
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-26 10:18:59 +0900

    Update 190326_BitwiseOperator.java

--- Java/190326_BitwiseOperator.java
+++ Java/190326_BitwiseOperator.java
@@ -44,3 +44,25 @@ class BitOp
 	}
 }
 // += -= *= /= %=
+
+//ex4
+
+class BitOp
+{
+	public static void main(String[] args) 
+	{
+		
+		int a=1;
+
+		a*=2;
+		a*=3;
+		a*=4;
+		a*=5;
+		a*=6;
+		a*=7;
+		a*=8;
+		a*=9;
+
+		System.out.println(a);
+	}
+}
