57d4bf8516b8b25b2916ecf3ab017d4854d2523c
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-26 10:35:16 +0900

    Update 190326_ConditionalTernaryOperator.java

--- Java/190326_ConditionalTernaryOperator.java
+++ Java/190326_ConditionalTernaryOperator.java
@@ -21,3 +21,27 @@ class Ternary
 		System.out.println(a);
 	}
 }
+
+//ex2
+
+import java.util.Scanner;
+
+class Ternary
+{
+	public static void main(String[] args) 
+	{
+		boolean result;
+		int num;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("숫자 입력 : ");
+		num = sc.nextInt();
+
+		result = num%2 == 0;
+
+
+		String a = result? "짝수 입니다.":"홀수 입니다.";
+		System.out.println(a);
+	}
+}
