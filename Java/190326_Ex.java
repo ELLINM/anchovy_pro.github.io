d422f94e0d86419251641f06cbac3121b410a6b9
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-26 16:52:30 +0900

    Update 190326_Ex.java

--- Java/190326_Ex.java
+++ Java/190326_Ex.java
@@ -112,7 +112,7 @@ class TypeCasting3
 		System.out.print("정수 입력 : ");
 		num = cc.nextInt();
 
-		sum = (num%10)+((num/10)%10)+(((num/10)/10)%10);
+		sum = (num%10)+((num/10)%10)+((num/110)%10);
 
 		System.out.println(sum);
 	}
