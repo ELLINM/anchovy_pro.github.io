7ca861f2f3c705ad11f46b36ad9c9ace6deaddc6
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-26 10:38:18 +0900

    Update 190326_ConditionalTernaryOperator.java

--- Java/190326_ConditionalTernaryOperator.java
+++ Java/190326_ConditionalTernaryOperator.java
@@ -30,18 +30,17 @@ class Ternary
 {
 	public static void main(String[] args) 
 	{
-		boolean result;
-		int num;
+		boolean isEven;//의미를 담은 변수명을 지어주는것이 좋음
+		int num; //입력 받은 숫자를 담는 변수
 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("숫자 입력 : ");
 		num = sc.nextInt();
 
-		result = num%2 == 0;
+		isEven = num%2 == 0; //짝수임을 판별 짝수 = true, 홀수  = false
 
-
-		String a = result? "짝수 입니다.":"홀수 입니다.";
+		String a = isEven? "짝수 입니다.":"홀수 입니다.";
 		System.out.println(a);
 	}
 }
