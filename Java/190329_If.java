251467050321f2fa85ce77dcaaf1e18f0fff716e
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-29 10:06:50 +0900

    Update 190329_If.java

--- Java/190329_If.java
+++ Java/190329_If.java
@@ -131,10 +131,10 @@ class Grade2
 		else if(score>=80){
 			System.out.println("B");
 		}
-		else if(score>=80){
+		else if(score>=70){
 			System.out.println("C");
 		}
-		else if(score>=70){
+		else if(score>=60){
 			System.out.println("D");
 		}
 		else{
