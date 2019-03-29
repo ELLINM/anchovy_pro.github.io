b9af06fab6daa277bf287c7792f221abca6577bd
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-29 18:25:31 +0900

    Update 190329_Ex.java

--- Java/19March/190329_Ex.java
+++ Java/19March/190329_Ex.java
@@ -132,3 +132,35 @@ class Int
 	}
 }
 
+//ex4
+
+import java.util.Scanner;
+
+class SwitchCase
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("분류코드를 입력하세요 : ");
+		Scanner sc = new Scanner(System.in);
+		int code = sc.nextInt();
+
+		switch (code)
+		{
+		case 1 :
+			System.out.println("철학입니다.");
+		break;
+		
+		case 2 :
+			System.out.println("종교 입니다.");
+		break;
+		
+		case 3 : 
+			System.out.println("사회과학 입니다.");
+		break;
+		
+		default :
+			System.out.println("기타입니다.");
+		break;
+		}
+	}
+}
