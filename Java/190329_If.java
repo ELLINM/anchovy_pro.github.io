93c50187cf730832e8f82fac9a0c1a644141675c
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-29 09:51:00 +0900

    Update 190329_If.java

--- Java/190329_If.java
+++ Java/190329_If.java
@@ -73,3 +73,30 @@ class Test2
 		}
 	}
 }
+
+//ex4
+
+import java.util.Scanner;
+
+class Exam
+{
+	public static void main(String[] args) 
+	{
+		int score;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("점수 입력:");
+		score = sc.nextInt();
+
+		if(score>=70){
+			System.out.println("합격입니다.");
+		}
+
+		else if(score>=50){
+			System.out.println("재시험입니다.");
+		}
+		else{
+			System.out.println("탈락입니다.");
+		}
+	}
+}
