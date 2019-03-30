20ca4843973e52094e47074508603644c8b5c57d
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-03-30 16:04:46 +0900

    Update 190330_Ex.java

--- Java/19March/190330_Ex.java
+++ Java/19March/190330_Ex.java
@@ -10,3 +10,62 @@
 ============소극장============
 [] [김철수] [] [] [] [] [] [] [] []*/
 
+import java.util.Scanner;
+
+class Ex1 
+{
+	public static void main(String[] args) 
+	{
+		int seat;
+		String name;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("=========소극장==========\n[][][][][][][][][][]");
+		
+		System.out.print("이름 입력 : ");
+		name = sc.nextLine();
+		
+		System.out.print("몇 번째 자리에 앉습니까? : ");
+		seat = sc.nextInt();
+
+		switch (seat)
+		{
+		case 1:
+			System.out.println("=========소극장==========\n["+name+"][][][][][][][][][]");
+			break;
+		case 2:
+			System.out.println("=========소극장==========\n[]["+name+"][][][][][][][][]");
+			break;
+		case 3:
+			System.out.println("=========소극장==========\n[][]["+name+"][][][][][][][]");
+			break;
+		case 4:
+			System.out.println("=========소극장==========\n[][][]["+name+"][][][][][][]");
+			break;
+		case 5:
+			System.out.println("=========소극장==========\n[][][][]["+name+"][][][][][]");
+			break;
+		case 6:
+			System.out.println("=========소극장==========\n[][][][][]["+name+"][][][][]");
+			break;
+		case 7:
+			System.out.println("=========소극장==========\n[][][][][][]["+name+"][][][]");
+			break;
+		case 8:
+			System.out.println("=========소극장==========\n[][][][][][][]["+name+"][][]");
+			break;
+		case 9:
+			System.out.println("=========소극장==========\n[][][][][][][][]["+name+"][]");
+			break;
+		case 10:
+			System.out.println("=========소극장==========\n[][][][][][][][][]["+name+"]");
+			break;
+		
+		default:
+			System.out.println("좌석을 제대로 입력해주세요");
+
+		}
+
+	}
+}
