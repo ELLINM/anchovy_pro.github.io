cf50f605aec5b1f13c07aa99886d1b5650231c19
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-02 09:46:18 +0900

    Update 190401_For.java

--- Java/190401_For.java
+++ Java/190401_For.java
@@ -36,7 +36,7 @@ class For1
 {
 	public static void main(String[] args) 
 	{
-		int x= 0; //x를 초기화
+		int x= 0; //x를 초기화 밖에서 출력하기 위해서는 밖에서 선언하는
 
 		for (int i = 1; i<=10; i++)//i는 1 -> 10보다 작거나 같을때까지 1씩 증가한다.
 		{//for (int i = 1; i<=10; i+=2)짝수값을 구할수 있다.
@@ -47,7 +47,7 @@ class For1
 			}
 			}을 사용 같은 값을 구할 수 있음*/
 			x+=i; // x와 i를 더해준다 i는 계속해서 1씩 증가하므로 0이었던 x에 1,2,3...10까지 더해진다.
-		}
+		}//For문 안에 출력문을 적으면 출력이 반복됨
 			System.out.println(x);
 	}
 }
