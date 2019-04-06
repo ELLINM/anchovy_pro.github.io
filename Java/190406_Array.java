afa6a5f2453f95cf6627cd54600095bca96fdcf9
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-06 15:02:30 +0900

    Update 190406_Array.java

--- Java/190406_Array.java
+++ Java/190406_Array.java
@@ -77,7 +77,9 @@ sArray[2] = "text";
 hArray[1] = "new Human();
 	
 //Array 선언, 생성, Initialization를 한번에
-String[] sArray = {"a", "b", "c", "d", "e",};//Array의 길이가 5인 String Type Array인 sArray를 선언하면서 동시에 각원소들을 주어진 값으로 초기화
+String[] sArray = {"a", "b", "c", "d", "e",};
+
+//Array의 길이가 5인 String Type Array인 sArray를 선언하면서 동시에 각원소들을 주어진 값으로 초기화
 
 //ex 6
 
@@ -86,42 +88,51 @@ class Person
 {
 	int age;
 	String name;
+	
 	// Array구성에 필요한 변수 선언
+	
 	public Person(int newAge, String newName)
 	{ 
-		//이후 사용하게될 Person Array의 요소들로 각각 선언해줌
+	//이후 사용하게될 Person Array의 요소들로 각각 선언해줌
+		
 		age = newAge;
 		name = newName;		
 	}
 
 	public String printAll() 
 	{ 
-		//출력 형식 선언
+	//출력 형식 선언
+		
 		return "Person [age = " + age + ", name = " + name + "]";
 	}
 }
 
-public class ArrayTest2// Array 구성
+public class ArrayTest2 // Array 구성
 {
 	public static void main(String [] args)
 	{ 
 		//Person Array 선언 및 생성
+		
 		Person[] pArray = new Person[3];
 		//각 Array의 방에 새로운 Person Object를 생성하여 Initialization
 		
 		pArray[0] = new Person(10, "Soft");
 		pArray[1] = new Person(20, "Engineer");
 		pArray[2] = new Person(30, "Society");
+		
 		/*pArray에는 각Index마다 요소들이 들어감
 		Array의 Index를 이용하여 값을 참조할 수 있다.*/
 		
 		System.out.println(pArray[0].name + pArray[1].name + pArray[2].name);
+		
 		/*처음에는 각 배열의 이름들이 출력이 됨 SoftEngineerSociety 이후에 배열을 출력하게됨 
 		Array의 length속성을 이용하여 Array를 순회할 수 있다.*/
 		
 		for (int i = 0; i < pArray.length; i++)
-		{/*각 배열을 출력해 주기 위해서 반복문을 사용하여 출력
+		{
+		/*각 배열을 출력해 주기 위해서 반복문을 사용하여 출력
 		Array의 길이는 3이기 때문에 0,1,2를 출력할 수 있음*/
+			
 			System.out.println(pArray[i].printAll());
 		}
 	}
