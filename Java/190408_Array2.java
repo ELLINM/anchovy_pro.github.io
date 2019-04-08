9138d6024819bfc7dfd5845907c74bf639e6d9bb
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-08 11:22:56 +0900

    Create 190408_Array2.java

---
+++ Java/190408_Array2.java
@@ -0,0 +1,17 @@
+//ex1
+
+
+class Array2
+{
+	public static void main(String[] args) 
+	{
+		int [] array = new int [10];
+	// 자료형  이름 메모리할당 데이터의 갯수
+		array[0] = 15; //선택 
+		
+		for (int i = 0; i<10; i++)
+		{
+			System.out.println(array[i]); //배열 전체를 출력하기 위한 함수
+		}
+	}
+}
