3836b6649d0350ad166d301e603961c1286af429
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-10 18:58:26 +0900

    Update 190409-10_Ex.java

--- Java/190409-10_Ex.java
+++ Java/190409-10_Ex.java
@@ -285,42 +285,6 @@ class Seat
 
 //ex8 같은 휴대전화는 예약 하지 못하게 하시오
 
-
-
-/*ex11 한 반에 30명인 학급이 있다. 각 학생의 점수를 입력받아, 학급의 평균을 구하는 프로그램을 만드시오. 
-매년 학생 수가 달라 질 수 있는데, 어떻게 하면 가장 적게 수고하여 매년 프로그램을 운영 할 수 있을지 고민하시오.*/
-
-
-import java.util.Scanner;
-
-class ClassRoom
-{
-	public static void main(String[] args) 
-	{
-		int [] student = new int [30];
-		int grade = 0, sum = 0;
-		double avg = 0;
-
-		Scanner sc = new Scanner(System.in);
-		
-		for (int i = 0; i <student.length; i++)
-		{
-			System.out.printf("%d번째 학생의 점수를 입력하세요 : \n", i+1);
-			grade = sc.nextInt();
-			student[i] = grade;
-		}
-		
-		for (int i = 0; i <student.length; i++)
-		{
-			sum = sum + student[i];
-		}
-			avg = (double)sum/student.length;
-		System.out.printf("학급평균 점수는 : %.2f 점 입니다.\n",avg);
-	}
-}
-/*학급의 인원수만 조정해 주면 되기 때문에 배열의 크기만 바꿔가면서 해주면 된다.*/
-
-
 /*ex12
 지금 까지 배운 것들을 합쳐 소극장 예약 프로그램을 매뉴화 시켜 만들어보자.
 (배열과 switch를 사용한다.)
