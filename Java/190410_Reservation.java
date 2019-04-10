a0df6a5fdc9721c62e8c7cca8921ccf538b9395f
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-10 18:59:53 +0900

    Create 190410_Reservation.java

---
+++ Java/190410_Reservation.java
@@ -0,0 +1,111 @@
+
+//teacher
+import java.util.Scanner;
+
+class ArrayTest
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String [] seats=new String [10];
+		String name;
+		boolean flag=true;
+		int choice,seatNum;	
+
+		while(flag){
+			
+			System.out.println("==============");
+			System.out.println("1.좌석예약");
+			System.out.println("2.좌석예약취소");
+			System.out.println("3.좌석정보보기");
+			System.out.println("4.좌석전체정보보기");
+			System.out.println("5.좌석초기화");
+			System.out.println("6.프로그램종료");
+			System.out.println("==============");
+			choice=sc.nextInt();
+			switch(choice){
+			case 1:
+
+				for(int i=0;i<seats.length;i++){
+					if(seats[i]==null){
+						System.out.print("[ ]" );
+					}else{
+						System.out.print("[O]" );
+					}
+				}
+				System.out.println();
+				System.out.println("예약할 사람의 이름을 입력 : ");
+				name=sc.next();
+				System.out.println("예약할 좌석번호 입력 (1~"+seats.length+")");
+				seatNum=sc.nextInt();
+
+				if(seats[seatNum-1]==null){
+					seats[seatNum-1]=name;
+				}else{
+					System.out.println("이미 예약이 되어 있습니다.");
+					System.out.println(seats[seatNum-1]);
+				}
+				break;
+			case 2:
+
+
+				for(int i=0;i<seats.length;i++){
+					if(seats[i]==null){
+						System.out.print("[ ]" );
+					}else{
+						System.out.print("[O]" );
+					}
+				}
+
+				System.out.println();
+				System.out.println("취소할 좌석번호 입력 (1~"+seats.length+")");
+				seatNum=sc.nextInt();
+
+				if(seats[seatNum-1]==null){
+					System.out.println("이미 비어 있습니다.");
+				}else{
+					seats[seatNum-1]=null;
+					System.out.println("취소완료");
+				}
+
+				break;
+			case 3:
+				System.out.println("정보보기 할 좌석번호 입력 (1~"+seats.length+")");
+				seatNum=sc.nextInt();
+				
+				if(seats[seatNum-1]==null){
+					System.out.println("비어있는 좌석입니다.");
+				}else{
+					System.out.println("예약자 이름 : "+seats[seatNum-1]);
+				}
+
+				break;
+			case 4:
+
+				for(int i=0;i<seats.length;i++){
+					if(seats[i]==null){
+						System.out.print("[ ]" );
+					}else{
+						System.out.print("[O]" );
+					}
+				}
+				System.out.println();
+
+			
+				break;
+			case 5:
+				for(int i=0;i<seats.length;i++){
+					seats[i]=null;
+				}
+				System.out.println("좌석 초기화 완료");
+				break;
+			case 6:
+				flag=false;
+				break;
+			
+			}
+
+		}
+		
+	}
+}
