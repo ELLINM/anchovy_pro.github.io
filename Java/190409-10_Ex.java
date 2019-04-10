84f249b58a4aa55615cde5c418e85030cc48133e
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-10 18:55:38 +0900

    Update 190409-10_Ex.java

--- Java/190409-10_Ex.java
+++ Java/190409-10_Ex.java
@@ -286,43 +286,6 @@ class Seat
 //ex8 같은 휴대전화는 예약 하지 못하게 하시오.
 
 
-/*ex9 철수는 올해 50권의 책을 읽으려고한다. 읽은 책 제목을 입력하면 
-String의 배열에 순차적으로 대입이 되며, 한 권 입력시, 한 번 전체 읽은 책 제목들이 출력이 된다. 
-프로그램으로 구현하여라.(철수를 위한 프로그램 만들기)*/
-
-
-import java.util.Scanner;
-
-class Books
-{
-	public static void main(String[] args) 
-	{
-		String book;
-		String [] books = new String [50];
-
-		Scanner sc = new Scanner(System.in);
-
-		while (true)
-		{
-			System.out.println("책제목을 입력하세요 : ");
-			book = sc.next();
-//책 제목을 입력받는 것까지 수행 그러나 전체 출력을 못함 
-		for (int i = 0; i < 50; i++)
-		{
-			books[i] = book;
-			if (books[i] == null)
-			{
-				System.out.print(" ");
-			}
-			else
-			{
-				System.out.println("[" + books[i] + "]");
-			}
-		}
-		}
-	}
-}
-
 
 /*ex10 D제약회사 제품 품질 관리에서는 1000개의 총 제품 수 중에 10개의 제품을 랜덤 샘플 품질 실험을 하여  
 2개가 품질 불합격을 받는 다면 1000개의 제품을 모두 폐기 하고 있다. 
