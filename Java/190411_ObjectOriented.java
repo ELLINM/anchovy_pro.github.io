3120d828badce4079c62a340f2887962e084a683
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-11 10:50:14 +0900

    Update 190411_ObjectOriented.java

--- Java/190411_ObjectOriented.java
+++ Java/190411_ObjectOriented.java
@@ -14,8 +14,23 @@ class ObjectTest
 	{
 		Human h =new Human(); //같은 폴더의 Human Class를 가지고옴
     
-    System.out.println(h.age);
+    		System.out.println(h.age);
 		System.out.println(h.name);
 	}
 }
 
+//ex3
+class ObjectTest 
+{
+	public static void main(String[] args) 
+	{
+		Human h = new Human(); //new하는 키워드가 중요 
+		Human x = h;
+
+		h.age = 25;
+		h.name = "홍길동";
+		x.age = 18;
+
+		System.out.println(x==h);
+	} //결과는 true 왜? 메모리 주소값이 같다고 판단하기 때문에 true가 나온다.
+}
