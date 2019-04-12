30c9851e0a04e611ee62cfdf1f23eae067c85c57
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-12 18:03:52 +0900

    Update 190412_ObjectOriented.java

--- Java/190412_ObjectOriented.java
+++ Java/190412_ObjectOriented.java
@@ -123,3 +123,100 @@ class Theater
 
 	}
 }
+
+
+//ex3
+
+//file 1
+class Customer 
+{
+	String name;
+	String phone;
+	String carNum;
+	String place;
+}
+
+
+//file 2
+
+import java.util.Scanner;
+
+class Drive
+{
+	public static void main(String[] args) 
+	{
+		int choice, counter = 0;
+		String name, phone, place, num;
+		Customer [] reserve = new Customer [10000];
+		Scanner sc = new Scanner(System.in);		
+		
+		while (true)
+		{
+			System.out.println("================");
+			System.out.println("1. 예약");
+			System.out.println("2. 조회");
+			System.out.println("3. 전체 출력");
+			System.out.println("================");
+			choice = sc.nextInt();
+			
+			switch (choice)
+			{
+			case 1:
+				System.out.println("이름 입력 : ");
+				name = sc.next();
+
+				System.out.println("핸드폰 번호 입력 : ");
+				phone = sc.next();
+
+				System.out.println("차량 번호 입력 : ");
+				num = sc.next();
+
+				System.out.println("장소 입력 : ");
+				place = sc.next();
+
+				boolean flag = true;
+	
+				Customer c = new Customer();
+				c.name = name;
+				c.phone = phone;
+				c.carNum = num;
+				c.place = place;
+
+				reserve[counter++] = c;
+				break;
+			case 2:
+				System.out.println("조회 위한 핸드폰 번호를 입력해 주세요");
+				phone = sc.next();
+				for (int i = 0; i < counter; i++)
+				{
+					if (reserve[i].phone.equals(phone))
+						{
+						flag = false; //한명이라도 같은 사람이 있으면
+							System.out.println("==================");
+							System.out.println("이름 : " + reserve[i].name);
+							System.out.println("전화번호 :" + reserve[i].phone);
+							System.out.println("차량번호 :" + reserve[i].carNum);
+							System.out.println("예약장소 : " + reserve[i].place);
+						}
+				} //for문 안에서 결과를 전부 도출 해내지 말것
+				if (flag)
+				{ //같은 사람이 없으면 true이기 때문에
+					System.out.println("검색결과가 없습니다.");
+				}
+				break;
+			case 3:
+				for (int i = 0; i < counter; i++)
+				{
+					System.out.println("==================");
+					System.out.println("이름 : " + reserve[i].name);
+					System.out.println("전화번호 :" + reserve[i].phone);
+					System.out.println("차량번호 :" + reserve[i].carNum);
+					System.out.println("예약장소 : " + reserve[i].place);
+					}
+				break;
+			}
+		}
+	}
+}
+
+
