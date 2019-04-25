7a6ab7c38b587d0a7eb6bc8df40486e7e5e29db5
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-25 09:52:08 +0900

    Create 190425_ArrayList.java

---
+++ Java/19April/190425_ArrayList.java
@@ -0,0 +1,16 @@
+package VO;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+	
+	public static void main(String [] args) {
+		
+		ArrayList<String> sList = new ArrayList<>();
+		//<>사용 입력하고싶은 data type입력 
+		sList.add("가");
+		//parameter로 넘겨줌
+		
+	}
+
+}
