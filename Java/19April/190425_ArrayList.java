6a0daa06526e73a06ff80859be6067daafbf5bc3
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-25 10:33:26 +0900

    Update 190425_ArrayList.java

--- Java/19April/190425_ArrayList.java
+++ Java/19April/190425_ArrayList.java
@@ -15,10 +15,24 @@ public class ArrayListTest {
 		sList.add("라");
 		//입력 순서대로 index부여
 		
-		System.out.println(sList.get(1));
+		/*System.out.println(sList.get(1));
 		
 		System.out.println(sList.size());
-		//최대길이 확인	
+		//최대길이 확인
+		
+		for (String str:sList) {
+			System.out.println(str);
+		}*/
+		
+		sList.remove(2); //다 삭데
+		sList.remove(2); //라 삭제
+		
+		sList.set(2, "마");//수정
+		
+		for (String str:sList) {
+			System.out.println(str);
+		}
+		
 	}
-}
 
+}
