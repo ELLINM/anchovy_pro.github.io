f669f60b05de76c7df0c1b14c923d05d82cd1872
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-25 18:34:20 +0900

    Create 190425_Maker.java

---
+++ Java/19April/190425_Maker.java
@@ -0,0 +1,14 @@
+pakage Main;
+
+public class WoodMaker; {
+
+  String wood = null;
+  
+  public void cutwood() {
+    wood = "막 자른 나무";
+  }
+  public void trimwood() {
+    if (wood!=null) {
+      wood = "다듬은 나무"
+      }
+  }
