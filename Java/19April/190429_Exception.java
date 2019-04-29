01d3ac980163545e4316e6a64f116398f6677b76
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-04-29 09:36:42 +0900

    Update 190429_Exception.java

--- Java/19April/190429_Exception.java
+++ Java/19April/190429_Exception.java
@@ -17,6 +17,7 @@ public class TestMain {
 		// 입력하는 순간 대입이 되는데 오류를 입력하게되면 대입을 하지않고 초기값을 가지고옴
 		System.out.println("입력성공");
 	}catch(InputMismatchException exc) {
+		//catch(Exception exc) 을 사용하여 모든 Exception을 처리 할 수 
 		num = 99;
 		System.out.println("입력오류");
 		//초기값을 설정해줄 수 있음
