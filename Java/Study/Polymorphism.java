2a5b8ab521d27ee072aacb528c67500a82240664
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-02 10:31:14 +0900

    Create Polymorphism.java

---
+++ Java/Study/Polymorphism.java
@@ -0,0 +1,8 @@
+Polymorphism
+================
++ 상속 관계에서 같은 기능이 여러 형태로 존재하는 것을 말한다.
++ 같은 기능을 하는 Method가 여러 Class에서 정의 될 수 있는 것
++ 다른 상황에서 다르게 작동한다. => Class의 내용에 따라 다르게 구현될 수 있음
+
++ ex)
+![Polymorphism ex](https://i1.wp.com/www.brightdevelopers.com/wp-content/uploads/2017/08/polymorphism-big.png?fit=562%2C382&ssl=1)
