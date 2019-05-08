c3a287d0fbad217dc134248a077381b5de0493fb
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-08 10:23:22 +0900

    Update 190508_ObjectStream2.java

--- Java/19May/190508_ObjectStream2.java
+++ Java/19May/190508_ObjectStream2.java
@@ -29,6 +29,10 @@ public class Person implements Serializable{
 	public void setAge(int age) {
 		this.age = age;
 	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
 }
 //직렬화를 해주지 않으면 알아볼수 없는 오류를 맛볼 수 있다.
 
