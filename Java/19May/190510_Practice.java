97cb3e88d2e4f69e9491040c04dd988861bad852
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-10 22:46:15 +0900

    Update 190510_Practice.java

--- Java/19May/190510_Practice.java
+++ Java/19May/190510_Practice.java
@@ -82,3 +82,108 @@ public class JavaPractice {
 		System.out.println("처리되었습니다.");
 	}
 }
+
+
+package Practice;
+
+public class JavaPractice {
+	
+	public static void main(String [] args)	{
+		boolean bl = false;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (bl == false) {
+					System.out.print("*");
+					bl = true; //*을 출력하고 true를 대입
+				}else {
+					System.out.print("-");
+					bl = false; // - 를 출력하고 false를 대입
+				}
+			}
+			System.out.println("\n"); //j의 5회 반복이 끝나면 i의 1회 이므로 for문을 나와서 다음줄로 넘어간다.
+		}
+	}
+}
+
+
+package Practice;
+
+import java.io.*;
+import java.io.InputStreamReader;
+
+public class JavaPractice {
+	
+	public static void main(String [] args)	throws IOException{
+		
+		System.out.println("몇 번째 처리를 전너 뛰시겠습니까? (1-10)");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+		
+		for (int i = 1; i <= 10; i++) {
+			if (i == res) 
+				continue; //if 문을 {로 묶지 않음으로서 처리한다.
+			System.out.println(i + "번째 처리입니다.");
+		}
+	}
+}
+
+
+package Practice;
+
+import java.io.*;
+
+public class JavaPractice {
+	
+	public static void main(String [] args)	throws IOException{
+		System.out.println("시험 응시자 수를 입력하십시오.");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str =  br.readLine();
+		int num = Integer.parseInt(str);
+		//배열의 요소 개수 입력
+		
+		int[] test;
+		test = new int[num];
+		// 필요한 개수만큼 배열요소를 준비
+		
+		System.out.println("시험 응시자 수 만큼 점수를 입력하십시오");
+		
+		for(int i = 0; i < num; i++) {
+			str = br.readLine();
+			int tmp = Integer.parseInt(str);
+			test[i] = tmp;
+		}
+		
+		for (int i = 0; i < num; i++) {
+			System.out.println((i + 1) + "번째 사람의 점수는  " + test[i] + "입니다.");
+		}
+	}
+}
+
+
+package Practice;
+
+import java.io.*;
+
+public class JavaPractice {
+	
+	public static void main(String [] args)	{
+		
+		try {
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("test1.txt")));
+			
+			pw.println("Hello");
+			pw.println("GoodBye");
+			System.out.println("파일에 기롣되었습니다.");
+			
+			pw.close();
+		}catch(Exception e){
+			System.out.println("입출력 에러가 발생하였습니다.");
+		}
+		
+	}
+}
