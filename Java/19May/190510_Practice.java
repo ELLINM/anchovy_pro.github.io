aa613b3c2ae9d6e4f9c93d05eb4758a71d757edc
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-13 08:40:59 +0900

    Update 190510_Practice.java

--- Java/19May/190510_Practice.java
+++ Java/19May/190510_Practice.java
@@ -187,3 +187,31 @@ public class JavaPractice {
 		
 	}
 }
+
+
+package Practice;
+
+class JavaPractice {
+	
+	void sub(int...v) {
+		System.out.println("인수의 개수 : " + v.length);
+		for(int x : v) {
+			System.out.println(x + " ");
+			System.out.println();
+		}
+	}
+}
+
+package Practice;
+
+import java.util.Scanner;
+
+public class VarArgsTest {
+	
+		public static void main(String [] args) {
+			JavaPractice c = new JavaPractice();
+			Scanner sc = new Scanner(System.in);
+			c.sub(sc.nextInt());
+	}
+}
+
