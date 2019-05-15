93c3e18ad3537c1c5a733160040966e400269e31
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-15 20:26:26 +0900

    Update 190515_Sheriff.java

--- Java/19May/190515_Sheriff.java
+++ Java/19May/190515_Sheriff.java
@@ -280,6 +280,40 @@ public class SheriffUI {
 }
 
 
+//Interface
+package service;
+
+import vo.Criminal;
+import vo.Person;
+import vo.Sheriff;
+
+public interface ServiceInterface {
+
+	public boolean insertPerson(Person p);//인적사항등록-범죄자나 보안관을 등록한다. 
+
+	public Person searchPersonBySsn(String ssn);//주민번호로 검색
+
+	public Sheriff searchSheriffByOfficerNum(String officerNum);//보안관번호로 검색
+
+	public Criminal searchCriminalByCriminalNum(String criminalNum);//범죄자번호로 검색
+
+	public boolean caughtCriminal(String officerNum, String criminalNum);//잡은보안관번호, 검거된 범죄자번호 입력 받아 맵핑
+
+	public String orderSheriffByHighCaught();//높은 검거율순으로 보안관 정보출력
+
+	public String orderCriminalByHighBounty();//높은 현상금순으로 범죄자 출력
+
+	public String unCaughtCriminalList();//미검거 범죄자 목록
+
+	public String printAll();//전체출력, 보안관->범죄자순
+
+	public void saveFile();//파일저장, 종료시 1회 저장
+
+	public void loadFile();//파일로드, 시작시 1번 로드
+
+}
+
+
 //Service
 
 package service;
@@ -313,7 +347,9 @@ public class SheriffService implements ServiceInterface {
 			if ((p instanceof Sheriff && searchSheriffByOfficerNum(((Sheriff) p).getOfficerNum()) == null)
 					|| ((p instanceof Criminal
 							&& searchCriminalByCriminalNum(((Criminal) p).getCriminalNum()) == null))) {
-
+		/*searchPersonBySsn(p.getSsn())값이 null이라면 p가 Sheriff or Criminal에 들어있는지 비교하고 
+				각각 search method에서 값을 리턴받아 null값인지 확인한다. 이러한 연산에서는 크게 or연산으로 진행하고
+				vo안에 있는지 확인하고 null값을 확인하기 위해서는 and연산으로 진행한다.*/
 				pList.add(p);
 				return true;
 			}
