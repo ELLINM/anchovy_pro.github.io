63bd6e4328d253a28cae93e303c60f65d0833acc
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-30 18:04:58 +0900

    Update 190529_Data.java

--- Java/19May/190529_Data.java
+++ Java/19May/190529_Data.java
@@ -6,12 +6,12 @@ public class DriverLoadingTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	try {
-		 Class.forName("oracle.jdbc.driver.OracleDriver");
-         System.out.println("드라이버 로딩 성공!");
-     } catch (ClassNotFoundException e) {
-         e.printStackTrace();
-     }
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+         		System.out.println("드라이버 로딩 성공!");
+     		} catch (ClassNotFoundException e) {
+        	 	e.printStackTrace();
+     		}
 	}
 }
 
