666bb1a267a50e57b89f5f3d2902a160e649ba47
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-05-30 18:39:22 +0900

    Update 190529_Data.java

--- Java/19May/190529_Data.java
+++ Java/19May/190529_Data.java
@@ -8,6 +8,15 @@ public class DriverLoadingTest {
 		// TODO Auto-generated method stub
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+		/*
+		 실행하여 "jdbc driver 로딩 성공"과 "오라클 연결 성공"이 나오면 정상적으로 완료
+		 위의 소스 코드 중 url의 jdbc:oracle:thin:@localhost:1521:xe는 본인의 오라클 주소에 해당하는 내용
+		 localhost는 접속할 데이터베이스의 ip 주소를 입력
+		 동일한 컴퓨터이면 자신의 ip를 의미하는 localhost를 작성
+		 1521은 리스너 포트 번호를 의미, 변경하지 않았으면 기본값이 1521
+		 xe는 SID를 의미
+		 SID를 모르면 system 계정으로 sqlplus에 로그인하여 select name from v$database; 를 입력하면 확인이 가능하
+		 user와 password는 본인이 접속할 오라클 계정의 아이디와 비밀번호*/
          		System.out.println("드라이버 로딩 성공!");
      		} catch (ClassNotFoundException e) {
         	 	e.printStackTrace();
