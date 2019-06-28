668a9f297836c5c21c5a56d433f2295dcbdac605
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-06-28 11:31:54 +0900

    Create _TodoListUI.java

---
+++ Java/19June/_190628/_ui/_TodoListUI.java
@@ -0,0 +1,15 @@
+//TodoListUI
+
+package global.sesoc.ui;
+
+import global.sesoc.manager.TodoListManager;
+
+public class TodoListUI {
+	TodoListManager manager;
+
+	
+	public TodoListUI(TodoListManager manager) {
+		this.manager = manager; 
+	}
+
+}
