915572e94be3f3c121ed233957f173684f276db0
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-06-28 11:34:43 +0900

    Create TodoListManager.java

---
+++ Java/19June/_190628/_dao/TodoListManager.java
@@ -0,0 +1,48 @@
+//TodoListManager
+
+package global.sesoc.manager;
+
+import java.util.List;
+
+import global.sesoc.vo.CheckValue;
+import global.sesoc.vo.MyTodoList;
+
+public class TodoListManager implements TotoList{
+
+	@Override
+	public int registTodo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteTodo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateTodo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<MyTodoList> selectOne(String today) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MyTodoList> selectAll(String today) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MyTodoList> selectAll(CheckValue checkValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
