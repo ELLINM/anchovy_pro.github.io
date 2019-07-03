c2ae32fffac17af21a6eab8fed954553dfd928de
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-03 10:21:34 +0900

    Update _SpringBasic.java

--- Web/190703/_SpringBasic.java
+++ Web/190703/_SpringBasic.java
@@ -1,6 +1,4 @@
-//Send Parameter
-
-package com.sesoc.web;
+package com.sesoc.web.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -12,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sesoc.web.vo.TestVO;
 
 /**
  * Handles requests for the application home page.
@@ -46,4 +46,73 @@ public class HomeController {
 
 		return "index";
 	}
+	
+	@RequestMapping(value = "send2", method = RequestMethod.GET)
+	public String send2(String a, int b) {
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+
+		return "index";
+	}
+	
+	@RequestMapping(value = "send3", method = RequestMethod.POST)
+	public String send3(String a, int b) {
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+
+		return "index";
+	}
+	
+	@RequestMapping(value = "send4", method = RequestMethod.GET)
+	public String send4(TestVO vo) {
+
+		System.out.println(vo);
+
+		return "index";
+	}
+}
+
+
+//TestVO
+
+package com.sesoc.web.vo;
+
+public class TestVO {
+	
+	private String a;
+	private int b;
+	
+	public TestVO() {
+	}
+
+	public TestVO(String a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "TestVO [a=" + a + ", b=" + b + "]";
+	}
+	
+	
 }
