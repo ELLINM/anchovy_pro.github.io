3e9e6afec6f4572086663b2586797c1741081c49
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-08 11:11:03 +0900

    Update _InsertCon.java

--- Web/190708/_InsertCon.java
+++ Web/190708/_InsertCon.java
@@ -87,6 +87,8 @@ public class MemberController {
 //TestCon
 package com.test.web.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -105,6 +107,18 @@ public class TestController {
 	@RequestMapping(value = "testInsert", method = RequestMethod.GET)
 	public String testInsert(TestVO vo) {
 		service.testInsert(vo);
+		return "main";
+	}
+	
+	@RequestMapping(value = "testSession1", method = RequestMethod.GET)
+	public String testSession1(HttpSession session) {
+		session.setAttribute("test", "세션값");
+		return "main";
+	}
+	
+	@RequestMapping(value = "testSession2", method = RequestMethod.GET)
+	public String testSession2(HttpSession session) {
+		session.removeAttribute("test");
 		return "main";
 	}
 }
