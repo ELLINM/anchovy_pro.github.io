b141b8d19dabebd5bd06f32e4d057f16de41d181
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-08 09:18:02 +0900

    Create _InsertDAO.java

---
+++ Web/190708/_InsertDAO.java
@@ -0,0 +1,62 @@
+package com.test.web.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.test.web.vo.MemberVO;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// 회원가입 처리
+	public int signup(MemberVO vo) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		return mapper.signup(vo);
+	}
+}
+
+
+
+package com.test.web.dao;
+
+import com.test.web.vo.MemberVO;
+
+public interface MemberMapper {
+	public int signup(MemberVO vo);
+}
+
+
+
+package com.test.web.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.test.web.vo.TestVO;
+
+@Repository
+public class TestDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int testInsert(TestVO vo) {
+		TestMapper mapper = sqlSession.getMapper(TestMapper.class);
+		return mapper.testInsert(vo);
+	}
+}
+
+
+
+package com.test.web.dao;
+
+import com.test.web.vo.TestVO;
+
+public interface TestMapper {
+	public int testInsert(TestVO vo);
+}
