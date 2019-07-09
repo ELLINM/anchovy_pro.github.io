7a9cd6691c6a930b3215c0a9e9b9b88fb5e8ee67
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-09 18:54:03 +0900

    Create _TestTestDao.java

---
+++ Web/190708/_TestTestDao.java
@@ -0,0 +1,30 @@
+//TestMapper
+package com.test.web.dao;
+
+import com.test.web.vo.TestVO;
+
+public interface TestMapper {
+	public int testInsert(TestVO vo);
+}
+
+
+//TestDao
+package com.test.web.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.test.web.vo.TestVO;
+
+@Repository
+public class TestDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int testInsert(TestVO vo) {
+		TestMapper mapper = sqlSession.getMapper(TestMapper.class);
+		return mapper.testInsert(vo);
+	}
+}
