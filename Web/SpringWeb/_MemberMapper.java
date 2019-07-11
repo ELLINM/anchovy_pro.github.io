3e6eefd86d711b9b1a445cc5cc2788f830767037
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-11 21:55:39 +0900

    Create _MemberMapper.java

---
+++ Web/SpringWeb/_MemberMapper.java
@@ -0,0 +1,10 @@
+//MemberMapper
+
+package com.test.web.dao;
+
+import com.test.web.vo.MemberVO;
+
+public interface MemberMapper {
+	public int signup(MemberVO vo);
+	public MemberVO login(MemberVO vo);
+}
