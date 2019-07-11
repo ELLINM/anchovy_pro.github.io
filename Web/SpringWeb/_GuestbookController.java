06cf2859b5371aa1ad14a7a30c004555207f8315
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-11 22:00:25 +0900

    Update and rename _TestGuestbookCon.java to _GuestbookController.java

--- Web/SpringWeb/_GuestbookController.java
+++ Web/SpringWeb/_GuestbookController.java
@@ -3,8 +3,6 @@ package com.test.web.controller;
 
 import java.util.ArrayList;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.test.web.service.GuestbookService;
-import com.test.web.service.MemberService;
 import com.test.web.vo.GuestbookVO;
-import com.test.web.vo.MemberVO;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -23,11 +19,18 @@ public class GuestbookController {
 	@Autowired
 	private GuestbookService service;
 	
+	// 방명록 리스트 화면 이동
 	@RequestMapping(value = "guestbookList", method = RequestMethod.GET)
 	public String guestbookList(Model model) {
 		ArrayList<GuestbookVO> list = service.guestbookList();
 		model.addAttribute("list", list);
 		return "/guestbook/guestbookList";
+	}
+	
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	public String delete(GuestbookVO vo) {
+		service.delete(vo);
+		return "redirect:/guestbook/guestbookList";
 	}
 	
 	@RequestMapping(value = "write", method = RequestMethod.POST)
@@ -36,11 +39,5 @@ public class GuestbookController {
 		return "redirect:/guestbook/guestbookList";
 	}
 	
-	@RequestMapping(value = "delete", method = RequestMethod.POST)
-	public String delete(GuestbookVO vo) {
-		service.delete(vo);
-		return "redirect:/guestbook/guestbookList";
-	}
-
+	
 }
-
