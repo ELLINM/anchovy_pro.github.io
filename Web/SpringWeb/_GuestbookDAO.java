9b3cec3f5bc4662051a86574d8579bce908e1626
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-11 21:56:38 +0900

    Update and rename _TestGuestbookDao.java to _GuestbookDAO.java

--- Web/SpringWeb/_GuestbookDAO.java
+++ Web/SpringWeb/_GuestbookDAO.java
@@ -1,20 +1,4 @@
-//GeustbookMapper
-
-package com.test.web.dao;
-
-import java.util.ArrayList;
-
-import com.test.web.vo.GuestbookVO;
-
-public interface GuestbookMapper {
-	public ArrayList<GuestbookVO> guestbookList();
-	public void write(GuestbookVO vo);
-	public void delete(GuestbookVO vo);
-}
-
-
-
-//GuestbookDao
+//GuestbookDAO
 
 package com.test.web.dao;
 
@@ -25,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.test.web.vo.GuestbookVO;
-import com.test.web.vo.MemberVO;
 
 @Repository
 public class GuestbookDAO {
@@ -33,20 +16,19 @@ public class GuestbookDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	// 방명록 리스트
 	public ArrayList<GuestbookVO> guestbookList() {
 		GuestbookMapper mapper = sqlSession.getMapper(GuestbookMapper.class);
 		return mapper.guestbookList();
 	}
 	
-	public void write(GuestbookVO vo){
+	public void write(GuestbookVO vo) {
 		GuestbookMapper mapper = sqlSession.getMapper(GuestbookMapper.class);
 		mapper.write(vo);
-	
 	}
 	
-	public void delete(GuestbookVO vo){
+	public void delete(GuestbookVO vo) {
 		GuestbookMapper mapper = sqlSession.getMapper(GuestbookMapper.class);
 		mapper.delete(vo);
-	
 	}
 }
