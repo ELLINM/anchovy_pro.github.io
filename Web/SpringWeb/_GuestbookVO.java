91303122fe29abd9d95e670d3edab938d724c885
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-11 21:50:14 +0900

    Create _GuestbookVO.java

---
+++ Web/SpringWeb/_GuestbookVO.java
@@ -0,0 +1,15 @@
+//GuestbookVO
+
+package com.test.web.vo;
+
+import lombok.Data;
+
+@Data
+public class GuestbookVO {
+
+	private int seq;
+	private String name;
+	private String pwd;
+	private String content;
+	private String regdate;
+}
