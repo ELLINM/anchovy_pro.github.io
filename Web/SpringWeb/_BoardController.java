8eb3c786aca3700ba4744643070a4bceca35f953
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-15 11:45:25 +0900

    Update _BoardController.java

--- Web/SpringWeb/_BoardController.java
+++ Web/SpringWeb/_BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.test.web.service.BoardService;
 import com.test.web.vo.BoardVO;
@@ -22,7 +23,7 @@ public class BoardController {
 	@Autowired
 	private BoardService service;
 	
-	@RequestMapping(value = "boardList", method = RequestMethod.GET)
+	@RequestMapping(value = "boardList", method = {RequestMethod.GET, RequestMethod.POST})
 	public String boardList(Model model) {
 		ArrayList<BoardVO> list = service.boardList();
 		model.addAttribute("list", list);
@@ -37,8 +38,37 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "boardDelete", method = RequestMethod.GET)
-	public String boardDelete(BoardVO vo, HttpSession session) {
-		service.boardDelete(vo, session);
+	public String boardDelete(BoardVO vo, HttpSession session, RedirectAttributes rttr) {
+		boolean result = service.boardDelete(vo, session);
+		rttr.addFlashAttribute("deleteResult", result);
+		return "redirect:/board/boardList";
+	}
+	
+	@RequestMapping(value = "boardUpdate", method = RequestMethod.POST)
+	public String boardUpdate(BoardVO vo, HttpSession session, RedirectAttributes rttr) {
+		boolean result = service.boardUpadte(vo, session);
+		rttr.addFlashAttribute("updateResult", result);
+		return "redirect:/board/boardRead?boardNum=" + vo.getBoardNum();
+		//boardRead로 넘어갈때 boardNum을 갖고가지 않기 때문에 같이 넘겨줘야 한다.
+	}
+	
+	@RequestMapping(value = "boardUpdateForm", method = RequestMethod.GET)
+	public String boardUpdateForm(int boardNum, Model model) {
+		BoardVO vo = service.boardRead(boardNum);
+		//특정글에 대한 정보를 가져오면 되기 대문에 boardRead를 불러온다.
+		model.addAttribute("vo", vo);
+		return "/board/boardUpdateForm";
+	}
+	
+	@RequestMapping(value = "boardWriteForm", method = RequestMethod.GET)
+	public String boardWriteForm() {
+		return "/board/boardWriteForm";
+	}
+	
+	@RequestMapping(value = "boardWrite", method = RequestMethod.POST)
+	public String boardWriteForm(BoardVO vo, HttpSession session, RedirectAttributes rttr) {
+		boolean result = service.boardWrite(vo, session);
+		rttr.addFlashAttribute("writeResult", result);
 		return "redirect:/board/boardList";
 	}
 }
