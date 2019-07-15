f3b49b9577a7c68df1dc7c5689bcf81cf3285ed6
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-15 11:42:54 +0900

    Update _GuestbookController.java

--- Web/SpringWeb/_GuestbookController.java
+++ Web/SpringWeb/_GuestbookController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.test.web.service.GuestbookService;
 import com.test.web.vo.GuestbookVO;
@@ -20,24 +22,26 @@ public class GuestbookController {
 	private GuestbookService service;
 	
 	// 방명록 리스트 화면 이동
-	@RequestMapping(value = "guestbookList", method = RequestMethod.GET)
-	public String guestbookList(Model model) {
-		ArrayList<GuestbookVO> list = service.guestbookList();
+	@RequestMapping(value = "guestbookList", method = {RequestMethod.GET, RequestMethod.POST})
+	public String guestbookList(
+			@RequestParam(value="searchItem", defaultValue="name") String searchItem
+			, @RequestParam(value="searchKeyword", defaultValue="") String searchKeyword, Model model) {
+		ArrayList<GuestbookVO> list = service.guestbookList(searchItem, searchKeyword);
 		model.addAttribute("list", list);
 		return "/guestbook/guestbookList";
 	}
 	
-	@RequestMapping(value = "delete", method = RequestMethod.POST)
-	public String delete(GuestbookVO vo) {
-		service.delete(vo);
-		return "redirect:/guestbook/guestbookList";
-	}
-	
 	@RequestMapping(value = "write", method = RequestMethod.POST)
-	public String write(GuestbookVO vo) {
-		service.write(vo);
-		return "redirect:/guestbook/guestbookList";
+	public String write(GuestbookVO vo, Model model) {
+		boolean result = service.write(vo);
+		model.addAttribute("writeResult", result);
+		return "forward:/guestbook/guestbookList";
 	}
 	
-	
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	public String delete(GuestbookVO vo, Model model, RedirectAttributes attr) {
+		boolean result = service.delete(vo);
+		model.addAttribute("deleteResult", result);
+		return "forward:/guestbook/guestbookList";
+	}	
 }
