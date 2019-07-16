cbaca5bdbdd47739fc52f5416a36ac7633a42da3
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-16 21:32:16 +0900

    Update _BoardDAO.java

--- Web/SpringWeb/_BoardDAO.java
+++ Web/SpringWeb/_BoardDAO.java
@@ -3,12 +3,14 @@
 package com.test.web.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.test.web.vo.BoardVO;
+import com.test.web.vo.ReplyVO;
 
 @Repository
 public class BoardDAO {
@@ -16,9 +18,9 @@ public class BoardDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	public ArrayList<BoardVO> boardList() {
+	public ArrayList<BoardVO> boardList(HashMap<String, String> map) {
 		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
-		return mapper.boardList();
+		return mapper.boardList(map);
 	}
 	
 	public BoardVO boardRead(int boardNum) {
@@ -40,5 +42,20 @@ public class BoardDAO {
 	public int boardWrite(BoardVO vo) {
 		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
 		return mapper.boardWrite(vo);
+	}
+	
+	public void replyWrite(ReplyVO vo) {
+		ReplyMapper mapper = sqlSession.getMapper(ReplyMapper.class);
+		mapper.replyWrite(vo);
+	}
+	
+	public ArrayList<ReplyVO> replyList(int boardNum){
+		ReplyMapper mapper = sqlSession.getMapper(ReplyMapper.class);
+		return mapper.replyList(boardNum);
+	}
+
+	public void replyUpdate(ReplyVO vo) {
+		ReplyMapper mapper = sqlSession.getMapper(ReplyMapper.class);
+		mapper.replyUpdate(vo);
 	}
 }
