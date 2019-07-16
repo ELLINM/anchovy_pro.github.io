29d532f060d909fa71ed2ff7cee71272231bc396
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-16 21:00:35 +0900

    Update _MemberMapper.java

--- Web/SpringWeb/_MemberMapper.java
+++ Web/SpringWeb/_MemberMapper.java
@@ -5,6 +5,7 @@ package com.test.web.dao;
 import com.test.web.vo.MemberVO;
 
 public interface MemberMapper {
+	//MemberMapper는 memberMapper.xml의 namespace="com.test.dao.MemberMapper"를 가져옴 sql과 java를 연결해줌
 	public int signup(MemberVO vo);
 	public MemberVO login(MemberVO vo);
 }
