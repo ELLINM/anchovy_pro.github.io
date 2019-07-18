014c7c0b240ade5240bf85185d1c65f4025e644a
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-18 11:40:27 +0900

    Update _BoardMapper.java

--- Web/SpringWeb/_BoardMapper.java
+++ Web/SpringWeb/_BoardMapper.java
@@ -3,13 +3,16 @@ package com.test.web.dao;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.ibatis.session.RowBounds;
+
 import com.test.web.vo.BoardVO;
 
 public interface BoardMapper {
-	public ArrayList<BoardVO> boardList(HashMap<String, String> map);
+	public ArrayList<BoardVO> boardList(HashMap<String, String> map, RowBounds rb);
 	public BoardVO boardRead(int boardNum);
 	public void hitCount(int boardNum);
 	public int boardDelete(BoardVO vo);
 	public int boardUpdate(BoardVO vo);
 	public int boardWrite(BoardVO vo);
+	public int getTotal(HashMap<String, String> map);
 }
