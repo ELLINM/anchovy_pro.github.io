caefc62b69b12f4ee218431932fab39404efbbde
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-24 22:48:45 +0900

    Create shopmain.java

---
+++ DataBase/SecondTest/ui/shopmain.java
@@ -0,0 +1,13 @@
+package global.sesoc.comshop.ui;
+
+/**
+ * 프로그램의 시작
+ */
+public class ShopMain {
+
+	public static void main(String[] args) {
+		//ShopUI 객체 생성하여 프로그램 시작
+		new ShopUI();
+	} 
+
+}
