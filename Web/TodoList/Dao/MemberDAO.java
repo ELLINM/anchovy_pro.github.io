74ec9b4e3d52582891f05f81970c7e66358bf449
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-07-29 09:15:40 +0900

    Create MemberDAO.java

---
+++ Web/TodoList/Dao/MemberDAO.java
@@ -0,0 +1,29 @@
+package com.todoList.web.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.todoList.web.vo.MemberVO;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public void signup(MemberVO vo) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		mapper.signup(vo);
+	}
+
+	public int login(MemberVO vo) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		return mapper.login(vo);
+	}
+
+	public void withdraw(MemberVO vo) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		mapper.withdraw(vo);
+	}
+}
