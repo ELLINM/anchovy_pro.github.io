5eb0579bd682656012efa2f554f5ea8d6af34312
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-08-08 11:48:37 +0900

    Update TestVO.java

--- Ajax/AjaxTest/TestVO.java
+++ Ajax/AjaxTest/TestVO.java
@@ -1,10 +1,15 @@
 package com.todoList.web.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestVO {
 	
 	private String id;
 	private String name;
+	
 }
