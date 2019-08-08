9157c8bc21a8072fad4a36e8a5c2d5d513d6e766
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-08-08 11:48:22 +0900

    Update HomeController.java

--- Ajax/AjaxTest/HomeController.java
+++ Ajax/AjaxTest/HomeController.java
@@ -1,6 +1,9 @@
 package com.todoList.web.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,5 +35,29 @@ public class HomeController {
 	@ResponseBody
 	public void test2(TestVO vo) {
 		System.out.println(vo);
+	}
+	
+	@RequestMapping(value = "test3", method = RequestMethod.POST)
+	@ResponseBody
+	public void test3(@RequestBody TestVO vo) {
+		System.out.println(vo);
+	}
+	
+	@RequestMapping(value = "test4", method = RequestMethod.POST)
+	@ResponseBody
+	public void test4(TestVO vo) {
+		System.out.println(vo);
+	}
+	
+	@RequestMapping(value = "test5", method = RequestMethod.POST)
+	@ResponseBody
+	public ArrayList<TestVO> test5() {
+		ArrayList<TestVO> list = new ArrayList<>();
+		list.add(new TestVO("아이디1", "이름1"));
+		list.add(new TestVO("아이디2", "이름2"));
+		list.add(new TestVO("아이디3", "이름3"));
+		list.add(new TestVO("아이디4", "이름4"));
+		
+		return list;
 	}
 }
