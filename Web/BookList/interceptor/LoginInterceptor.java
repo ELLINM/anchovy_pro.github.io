31035b6076f1b6ce387c201cb0153a01950da717
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-08-13 19:57:41 +0900

    Update LoginInterceptor.java

--- Web/BookList/interceptor/LoginInterceptor.java
+++ Web/BookList/interceptor/LoginInterceptor.java
@@ -15,12 +15,17 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		
 		// 로그인 여부 확인
 		HttpSession session = request.getSession();
+		//HttpServletRequest에 저장되어 있는 session을 갖고옴
 		String loginId = (String)session.getAttribute("loginId");
+		//login이 되어 있다면 login되어 있는 id를 String값으로 갖고와서 저잘
 		
 		// 로그인을 안했으면 로그인 화면으로 리다이렉트
 		if(loginId == null) {
+			//loginId에 값이 들어있지 않는다면
 			String contextPath = request.getContextPath();
+			//String 값으로 저장
 			response.sendRedirect(contextPath+"/login");
+			//요청 응답으로 response로 담아서 보냄 login이 안되기 때문에 값이 들어있지 않음
 			return false;
 		}
 		
