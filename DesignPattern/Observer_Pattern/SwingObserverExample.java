e829214fae8dad19348d45a97055af46e1daf7c0
ellinm <46395859+ELLINM@users.noreply.github.com>
2019-08-24 16:23:15 +0900

    Update SwingObserverExample.java

--- DesignPattern/Observer_Pattern/SwingObserverExample.java
+++ DesignPattern/Observer_Pattern/SwingObserverExample.java
@@ -18,11 +18,14 @@ public class SwingObserverExample {
 		JButton button = new JButton("정말 해도될까?");
 		button.addActionListener(new AngelListener());
 		button.addActionListener(new DevilListener());
+		//AngelListener와 DevilListener를 Observer로 만듬
 		frame.getContentPane().add(BorderLayout.CENTER, button);
 		frame.setSize(200, 200);
 		frame.setVisible(true);
 	}
 	
+	
+	//Observer Class들은 내부 class형태로 정의
 	class AngelListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			System.out.println("안돼. nononono");
@@ -34,4 +37,5 @@ public class SwingObserverExample {
 			System.out.println("당연하지. gogogo");
 		}
 	}
+	//여기에서는 주제의 상태가 변경 되었을 때 update()가 아닌 actionPerformed() Method가 
 }
